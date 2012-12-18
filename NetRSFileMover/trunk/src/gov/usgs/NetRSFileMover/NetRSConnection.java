e491a08aa70081831c93a32cb0e36c186f197cd1
tparker <tparker@b23e0666-5c4d-0410-a39a-a125d005d78a>
2012-12-18 15:40:46 +0000

    Nothing to see here yet. Just moving code.

---
+++ NetRSFileMover/trunk/src/gov/usgs/NetRSFileMover/NetRSConnection.java
@@ -0,0 +1,130 @@
+package gov.usgs.NetRSFileMover;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.io.CopyStreamEvent;
+import org.apache.commons.net.io.CopyStreamListener;
+
+/**
+ * A class to hold a FTP connection to a NetRS
+ * 
+ * @author Tom Parker
+ * 
+ */
+public class NetRSConnection {
+	private NetRSSettings settings;
+	private long pollTime;
+	private final long startTime;
+	private FTPClient ftp;
+	private SimpleDateFormat fileNameFormat;
+
+	public NetRSConnection(NetRSSettings settings) {
+
+		this.settings = settings;
+		startTime = System.currentTimeMillis();
+		pollTime = startTime - (startTime % (settings.duration * 1000 * 60));
+		fileNameFormat = new SimpleDateFormat(settings.fileNameFormat);
+
+		ftp = new FTPClient();
+		ftp.setControlKeepAliveReplyTimeout(3 * 1000);
+		ftp.setControlKeepAliveTimeout(30 * 1000);
+		if (settings.printHash)
+			ftp.setCopyStreamListener(getCopyStreamListener());
+	}
+
+	private void connect() throws IOException {
+		if (ftp != null && ftp.isConnected())
+			return;
+
+		try {
+			ftp.connect(settings.address);
+		} catch (IOException e) {
+			if (ftp.isConnected())
+				ftp.disconnect();
+
+			throw e;
+		}
+	}
+
+	/**
+	 * Poll the next file from the receiver, updating pollTime
+	 * 
+	 * @throws IOException
+	 */
+	public void poll() {
+
+		// Stop polling if I've been running too long.
+		if (System.currentTimeMillis() > startTime + settings.duration * 1000
+				* 60) {
+			pollTime = Long.MIN_VALUE;
+			return;
+		}
+
+		String filename = fileNameFormat.format(new Date(pollTime));
+
+		// Just return if I already have the file or have gone too far back
+		if (polledLast() || new File(filename).exists())
+			return;
+
+		pollTime -= settings.duration * 1000 * 60;
+		FileOutputStream output = null;
+		try {
+			connect();
+			output = new FileOutputStream(filename);
+			ftp.retrieveFile(filename, output);
+		} catch (IOException e) {
+			System.err.println("Cannot retreive " + filename
+					+ ". Undeterred I will continue.");
+		} finally {
+			if (output != null)
+				try {
+					output.close();
+				} catch (IOException e) {
+				}
+		}
+
+		if (settings.depthFirst && !polledLast())
+			poll();
+	}
+
+	/**
+	 * Decide if I've checked for all files.
+	 * 
+	 * @return true if I've polled all files
+	 */
+	public boolean polledLast() {
+		return pollTime < startTime
+				- (settings.maxBackfill * 1000 * 60 * 60 * 24);
+	}
+
+	/**
+	 * create a CopyStreamListener to print hash marks durring downloads. Based
+	 * on FTPClient example code.
+	 * 
+	 * @return a hash-printing CopyStreamListener
+	 */
+	private static CopyStreamListener getCopyStreamListener() {
+		return new CopyStreamListener() {
+			private long mBytesTotal = 0;
+
+			public void bytesTransferred(CopyStreamEvent event) {
+				bytesTransferred(event.getTotalBytesTransferred(),
+						event.getBytesTransferred(), event.getStreamSize());
+			}
+
+			public void bytesTransferred(long totalBytesTransferred,
+					int bytesTransferred, long streamSize) {
+				long mBytes = totalBytesTransferred / (1024 * 1024);
+				for (long l = mBytesTotal; l < mBytes; l++) {
+					System.out.print("#");
+				}
+				mBytesTotal = mBytes;
+			}
+		};
+	}
+}
--- NetRSFileMover/trunk/src/gov/usgs/NetRSFileMover/NetRSFileMover.java
+++ NetRSFileMover/trunk/src/gov/usgs/NetRSFileMover/NetRSFileMover.java
@@ -1,121 +1,60 @@
 package gov.usgs.NetRSFileMover;
 
-
 import gov.usgs.util.ConfigFile;
-import gov.usgs.util.Util;
 
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
 import java.util.List;
-
-import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPReply;
-import org.apache.commons.net.io.CopyStreamEvent;
-import org.apache.commons.net.io.CopyStreamListener;
 
 /**
  * Retrieve files from a Trimble NetRS device via FTP
  * 
  * @author Tom Parker
  */
-public final class NetRSFileMover
-{
-	
-	private boolean printHash;
-	private ConfigFile cf;
-	private FTPClient ftp;
-	private ConfigFile configDefault;
-	private List<NetRSSettings> receiverSettings;
-	
-	public NetRSFileMover(ConfigFile cf)
-	{
-		configDefault = cf.getSubConfig("default");
-		List<String> receivers = cf.getList("receivers");
+public final class NetRSFileMover {
+	public static final boolean DEFAULT_BACKFILL_FIRST = true;
+	public static final int DEFAULT_MAX_RUNTIME = 1440;
+	public static final long ONE_DAY = 1000 * 60 * 60 * 24;
+
+	private List<NetRSConnection> receivers;
+
+	/**
+	 * simple constructor
+	 * 
+	 * @param cf
+	 */
+	public NetRSFileMover(ConfigFile cf) {
 		
-		for (String receiver : receivers)
-			receiverSettings.add(new NetRSSettings(receiver, configDefault.getSubConfig(receiver, true)));
-		
-		ftp = new FTPClient();
-        if (printHash) {
-            ftp.setCopyStreamListener(getCopyStreamListener());
-        }
+		for (String receiver : cf.getList("receivers")) {
+			
+			NetRSSettings settings = new NetRSSettings(receiver,
+					cf.getSubConfig(receiver, true));
+			
+			NetRSConnection connection = new NetRSConnection(settings);
+			receivers.add(connection);
+		}
 	}
-		
+
+	/**
+	 * Do the work
+	 */
 	private void go() {
-		
-//		long now = (System.currentTimeMillis() / 1000 / 60) / duration;
-//		long time = now;
-//		while (time > (now - maxBackfill / 24 / 60))
-//		{
-//			getFile(time);
-//			time -= duration;
-//		}
+		while (!receivers.isEmpty()) {
+			for (NetRSConnection receiver : receivers) {
+				receiver.poll();
+
+				if (receiver.polledLast())
+					receivers.remove(receiver);
+			}
+		}
 	}
-	
-	private void getFile(long time) {
-		
-//		StringBuilder format = new StringBuilder();
-//		format.append("/yyyyMM/");
-//		
-//		if (usePerDaySubdirectories)
-//			format.append("dd/");
-//		
-//		if (usePerSessionIdSubdirectories)
-//			format.append("'" + sessionId + "'/");
-//		
-//		format.append("'" + systemName + "'yyyyMMddhhMM'" + sessionId + "." + dataFormat);
-//		
-//		for (String receiver : receivers)
-//		{
-//			try {
-//				ftp.connect(receiver);
-//			} catch (IOException e) {
-//		        if (ftp.isConnected())
-//		        {
-//		            try {
-//		                ftp.disconnect();
-//		            } catch (IOException f) {}
-//		        }
-//		        System.err.println("Could not connect to " + receiver + ".");
-//			}
-//			
-//			int reply = ftp.getReplyCode();
-//			System.out.println("Connected to " + receiver);
-//			if (!FTPReply.isPositiveCompletion(reply))
-//			{
-//				try {
-//					ftp.disconnect();
-//				} catch (IOException e) {}
-//				
-//				System.err.println("FTP server refused connection.");
-//				System.exit(1);
-//			}
-//		}
-	}
-	
-	public static void main(String[] args) throws UnknownHostException {
-		ConfigFile cf =  new ConfigFile(args[0]);
+
+	/**
+	 * Main method. Always a good place to start.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ConfigFile cf = new ConfigFile(args[0]);
 		NetRSFileMover arch = new NetRSFileMover(cf);
 		arch.go();
-    }
-	
-    private static CopyStreamListener getCopyStreamListener() {
-        return new CopyStreamListener(){
-            private long mBytesTotal = 0;
-            public void bytesTransferred(CopyStreamEvent event) {
-                bytesTransferred(event.getTotalBytesTransferred(), event.getBytesTransferred(), event.getStreamSize());
-            }
-
-            public void bytesTransferred(long totalBytesTransferred,
-                    int bytesTransferred, long streamSize) {
-                long mBytes = totalBytesTransferred / (1024 * 1024);
-                for (long l = mBytesTotal; l < mBytes; l++) {
-                    System.out.print("#");
-                }
-                mBytesTotal = mBytes;
-            }
-        };
-    }
+	}
 }
-
