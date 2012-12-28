e964e47aa5ec4b2511553c925dc45f8bc93586e8
tparker <tparker@b23e0666-5c4d-0410-a39a-a125d005d78a>
2012-12-28 22:47:45 +0000

    Intermediate commit on days work to change SVN props

--- NetRSFileMover/trunk/src/gov/usgs/NetRSFileMover/NetRSConnection.java
+++ NetRSFileMover/trunk/src/gov/usgs/NetRSFileMover/NetRSConnection.java
@@ -1,9 +1,9 @@
-package gov.usgs.NetRSFileMover;
-
+package gov.usgs.netRSFileMover;
 
 import gov.usgs.util.Log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -17,58 +17,83 @@ import org.apache.commons.net.io.CopyStreamEvent;
 import org.apache.commons.net.io.CopyStreamListener;
 
 /**
- * A class to hold a FTP connection to a NetRS
+ * A class to hold a FTP connection to a NetRS.
  * 
  * @author Tom Parker
  * 
  */
 public class NetRSConnection {
-	private final static Logger LOGGER = Log.getLogger(NetRSConnection.class
+	private static final int ONE_MINUTE = 1000 * 60;
+	private static final int ONE_DAY = ONE_MINUTE * 60 * 24;
+
+	private static final Logger LOGGER = Log.getLogger(NetRSConnection.class
 			.getName());
 
-	private static int ONE_MINUTE = 1000 * 60;
-	private static int ONE_DAY = ONE_MINUTE * 60 * 24;
+	private final NetRSSettings settings;
+	private final SimpleDateFormat fileNameFormat;
 
-	private NetRSSettings settings;
+	private FTPClient ftp;
+
 	private long pollTime;
 	private final long endTime;
-	private final long oldestPoll;
-	private FTPClient ftp;
-	private SimpleDateFormat fileNameFormat;
+	private final long oldestPollTime;
 
+	/**
+	 * Simple constructor
+	 * 
+	 * @param settings
+	 */
 	public NetRSConnection(NetRSSettings settings) {
 
 		this.settings = settings;
-		
-		long startTime = System.currentTimeMillis();
-		endTime = startTime + (settings.duration * ONE_MINUTE);
-		pollTime = startTime - (startTime % (settings.duration * ONE_MINUTE));
-		oldestPoll = startTime - (settings.maxBackfill * ONE_DAY);
-		
+
 		fileNameFormat = new SimpleDateFormat(settings.fileNameFormat);
 		fileNameFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
-		LOGGER.info("first poll at "
+
+		long startTime = System.currentTimeMillis();
+		endTime = startTime + (settings.duration * ONE_MINUTE);
+
+		// Find start of current file and backup one interval. Do not attempt to
+		// transfer the file that is currently being written.
+		pollTime = startTime - (startTime % (settings.duration * ONE_MINUTE));
+		pollTime -= settings.duration * ONE_MINUTE;
+
+		// Retrieve files that are at most this old
+		oldestPollTime = pollTime - (settings.maxDays * ONE_DAY);
+
+		LOGGER.info("Will retreive files from  "
+				+ fileNameFormat.format(new Date(oldestPollTime)) + " to "
 				+ fileNameFormat.format(new Date(pollTime)));
 
 		ftp = new FTPClient();
-		
+
 		ftp.setConnectTimeout(settings.connectTimeout * 1000);
 		if (settings.printHash)
 			ftp.setCopyStreamListener(getCopyStreamListener());
 	}
 
+	/**
+	 * Connect to NetRS, setting receive window if needed
+	 * 
+	 * @throws IOException
+	 */
 	private void connect() throws IOException {
 
-		LOGGER.info("Connecting to " + settings.address);
+		LOGGER.fine("Connecting to " + settings.address);
 		try {
-			System.out.println("bps " + settings.bytesPerSecond);
-			if (settings.bytesPerSecond > 0)
-				ftp.setReceiveBufferSize(settings.bytesPerSecond/4);
-			
+			if (settings.bytesPerSecond > 0) {
+				int bufferSize = Math.min(settings.bytesPerSecond, 1500);
+				ftp.setReceiveBufferSize(bufferSize);
+			}
+
 			ftp.connect(settings.address);
 			ftp.login(settings.userName, settings.password);
-			ftp.enterLocalPassiveMode();
-			
+
+			if (settings.passiveFTP)
+				ftp.enterLocalPassiveMode();
+			else
+				ftp.enterLocalActiveMode();
+
 		} catch (IOException e) {
 			if (ftp.isConnected())
 				ftp.disconnect();
@@ -92,54 +117,89 @@ public class NetRSConnection {
 		}
 
 		String filename = fileNameFormat.format(new Date(pollTime));
-		LOGGER.fine("Polling " + settings.systemName + " for " + filename);
+		LOGGER.info("Polling " + settings.systemName + " for " + filename);
 
 		pollTime -= settings.duration * ONE_MINUTE;
-		OutputStream output = null;
-		boolean result = false;
-		File out = new File(settings.outputDir + File.separator
+
+		File outFile = new File(settings.outputDir + File.separator
 				+ settings.systemName + File.separator + filename);
 
-		// Just return if I already have the file or have gone too far back
-		if (out.exists()) {
+		// Just return if I already have the file
+		if (outFile.exists()) {
 			LOGGER.fine("I already have " + filename + " skipping it.");
 			return;
-		} else {
-			LOGGER.fine("I don't have " + filename + " looking for it.");
 		}
 
 		try {
 			connect();
-			out.getParentFile().mkdirs();
-			output = new FileOutputStream(out);
-			if (settings.bytesPerSecond > 0)
-				output = new ThrottledOutputStream(output,
-						settings.bytesPerSecond);
-			long now = System.currentTimeMillis();
-			result = ftp.retrieveFile(filename, output);
-			if (result)
-				LOGGER.fine("got file in " + (System.currentTimeMillis() - now)
-						+ " ms");
-			else
-				LOGGER.fine("didn't get file.");
-
-		} catch (IOException e) {
-			LOGGER.info(e.toString());
-			LOGGER.info("Cannot retreive " + filename
-					+ ". Undeterred I will continue.");
-		} finally {
-			if (output != null)
-				try {
-					output.close();
-					if (!result)
-						out.delete();
-					ftp.disconnect();
-				} catch (IOException e) {
-				}
+		} catch (IOException e1) {
+			LOGGER.warning("Could not connect to " + settings.systemName);
 		}
+
+		getFile(filename, outFile);
 
 		if (settings.depthFirst && !polledLast())
 			poll();
+
+		if (ftp.isConnected())
+			try {
+				ftp.disconnect();
+			} catch (IOException e) {
+			}
+	}
+
+	/**
+	 * Pull a single file from the receiver
+	 * 
+	 * @param remoteFile
+	 * @param localFile
+	 * @return true if file was retrieved
+	 * @throws IOException
+	 */
+	private void getFile(String remoteFile, File outFile) {
+
+		// download to a temp file to help avoid exposing partial files
+		File tmpFile = new File(settings.outputDir + File.separator + "tmp"
+				+ File.separator + settings.systemName + ".tmp");
+		tmpFile.getParentFile().mkdirs();
+
+		OutputStream output;
+		try {
+			output = new FileOutputStream(tmpFile);
+		} catch (FileNotFoundException e1) {
+			LOGGER.warning("Can't create temp file " + tmpFile);
+			return;
+		}
+
+		if (settings.bytesPerSecond > 0)
+			output = new ThrottledOutputStream(output, settings.bytesPerSecond);
+
+		long now = System.currentTimeMillis();
+
+		boolean result = false;
+		try {
+			result = ftp.retrieveFile(remoteFile, output);
+		} catch (IOException e) {
+			LOGGER.warning("Couldn't retrieve " + remoteFile);
+			e.printStackTrace();
+		}
+
+		if (output != null)
+			try {
+				output.close();
+			} catch (IOException e) {
+			}
+
+		if (result) {
+			LOGGER.fine("got file in " + (System.currentTimeMillis() - now)
+					+ " ms");
+			tmpFile.renameTo(outFile);
+		} else {
+			tmpFile.delete();
+			LOGGER.info("Couldn't get file. Server replied: "
+					+ ftp.getReplyString());
+			LOGGER.info("Undeterred I will continue.");
+		}
 	}
 
 	/**
@@ -148,11 +208,11 @@ public class NetRSConnection {
 	 * @return true if I've polled all files
 	 */
 	public boolean polledLast() {
-		return pollTime < oldestPoll;
+		return pollTime < oldestPollTime;
 	}
 
 	/**
-	 * create a CopyStreamListener to print hash marks durring downloads. Based
+	 * create a CopyStreamListener to print hash marks during downloads. Based
 	 * on FTPClient example code.
 	 * 
 	 * @return a hash-printing CopyStreamListener
